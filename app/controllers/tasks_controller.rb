82bb3231b8ca06c3fce5288e6113f368634be1c8
Maclin Phil <philmaclin@gmail.com>
2016-12-05 13:46:50 -0600

    Add categories to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,12 @@ class TasksController < ApplicationController
   before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.order("created_at DESC")
+    if params[:category].blank?
+      @tasks = Task.all.order("created_at DESC")
+    else
+      @category_id  = Category.find_by(name: params[:category]).id
+      @tasks = Task.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -28,7 +33,7 @@ class TasksController < ApplicationController
   end
 
   def update
-    if @task.update(task_params)
+    if @task.update(tasks_params)
       redirect_to @task
     else
       render "Edit"
@@ -42,8 +47,8 @@ class TasksController < ApplicationController
 
   private
 
-  def task_params
-    params.require(:task).permit(:title, :description, :company, :url)
+  def tasks_params
+    params.require(:task).permit(:title, :description, :company, :url, :category_id)
   end
 
   def find_task
